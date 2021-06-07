4c7fbe08833431e053d5aaaa7f63b9451ba71c1c
Camilla Vidot <camillavidot@gmail.com>
2021-06-07 16:43:26 +0100

    first commit

---
+++ src/components/EmailView/EmailView.js
@@ -0,0 +1,12 @@
+import Toolbar from '../EmailView/Toolbar'
+import Content from '../EmailView/Content'
+
+function EmailView() {
+  return (
+    <main className="email-view">
+      <Toolbar />
+      <Content />
+    </main>
+  )
+}
+export default EmailView
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import HeaderLeftMenu from '../Header/HeaderLeftMenu'
+import Search from '../Header/Search'
+
+function Header() {
+  return (
+    <header className="header">
+      <HeaderLeftMenu />
+      <Search />
+    </header>
+  )
+}
+export default Header
